fa4ed64058e640184f49591e94786542861564ee
Jefelyn2222 <hskshejejeje@gmail.com>
2025-02-13 21:20:11 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openEnvelope() {
+    document.querySelector(".flap").style.transform = "rotateX(180deg)";
+    document.querySelector(".letter").style.transform = "translateY(-10px)";
+    document.querySelector(".letter").style.opacity = "1";
+    document.querySelector(".flower").style.opacity = "1";
+    document.querySelector(".flower").style.transform = "scale(1)";
+}
